08c0803879d59a1efb61279a8dd8e03778291f16
Yevhenii <zhenia1717@gmail.com>
2022-08-29 00:33:32 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+//  Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+
+    if (iconMenu.classList.contains('_active')) {
+        document.body.classList.remove('_lock');
+        iconMenu.classList.remove('_active');
+        menuBody.classList.remove('_active');
+    }
+};
+
+const activePage = window.location.pathname;
+const menuLinks = document.querySelectorAll('ul li a').forEach(link => {
+    if (link.href.includes(`${activePage}`)) {
+        link.classList.add('active');
+    }
+})
+
+// const menuList = document.getElementsByClassName("menu__list");
+// const menuItem = document.getElementsByClassName("menu__item")
+// for (var i = 0; i < menuItem.length; i++) {
+//     menuItem[i].addEventListener("click", function () {
+//         var current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//     });
+// }
+
+
+
+// const menuLink = document.querySelector('.menu__link');
+// if (menuLink) {
+//     menuLink.addEventListener("click", function (e) {
+//         menuLink.classList.toggle('active')
+//     });
+
+// }
+
+// $(document).on('click', 'ul li', function () {
+//     $(this).addClass('.active').siblings().removeClass('.active');
+// });
